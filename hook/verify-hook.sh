0e4d30578b481301352e91657a11b0becb4fd20c
Tao Chen <iamtaochen@outlook.com>
2025-02-01 00:11:22 +0800

    optimize

--- hook/verify-hook.sh
+++ hook/verify-hook.sh
@@ -10,9 +10,13 @@ echo "ZEROSSL_HTTP_FV_CONTENT: $ZEROSSL_HTTP_FV_CONTENT"
 nginx_bin=$(which nginx)
 echo "nginx_bin: $nginx_bin"
 
+if [ "$ZEROSSL_HTTP_FV_PORT" = "" ] || [ "$ZEROSSL_HTTP_FV_PORT" = "0" ]; then
+    ZEROSSL_HTTP_FV_PORT=80
+fi
+
 file_content=$(echo "$ZEROSSL_HTTP_FV_CONTENT" | tr -d '\r' | awk '{printf "%s\\n", $0}')
 echo "file_content: $file_content"
-cat <<EOF > /etc/nginx/conf.d/verify.conf
+cat <<EOF >/etc/nginx/conf.d/verify.conf
 server {
     listen $ZEROSSL_HTTP_FV_PORT default_server;
     listen [::]:$ZEROSSL_HTTP_FV_PORT default_server;
